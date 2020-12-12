5a808eceb6356835686d4306689b831e2317919d
ajakcyer <ajakcyer97@gmail.com>
2020-12-11 23:12:08 -0500

    added create and patch actions on Rating controller

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -9,5 +9,31 @@ class Api::V1::RatingsController < ApplicationController
         rating = Rating.find(params[:id])
         render json: rating
     end
+
+    def create
+        rating = Rating.create!(rating_params)
+        byebug
+
+        if rating.valid?
+            render json: rating
+        else
+            render json: {error: "Invalid POST request"}
+        end
+    end
+
+    def patch
+        rating = Rating.find(params[:id])
+
+        byebug
+
+        rating.update(rating_params)
+        render json: rating
+    end
+
+    private
+
+    def rating_params
+        params.require(:rating).permit(:user_id, :opinion_id, :agreeable)
+    end
     
 end
